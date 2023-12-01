f3d8ca21ed2b34aad9ff6cd85ee0e9c1b4bc0717
Moodyw03 <82095659+Moodyw03@users.noreply.github.com>
2023-12-01 12:50:07 +0000

    Add CRUD routes for Notes

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+import os
+import datetime
 
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+from bson import ObjectId
+from controllers.note_controller import create_note, get_notes, get_note, update_note, delete_note
+
+
+from config.db import db
+
+load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+
+
+# CRUD Routes for Notes
+app.route('/notes', methods=['POST'])(create_note)
+app.route('/notes', methods=['GET'])(get_notes)
+app.route('/notes/<string:note_id>', methods=['GET'])(get_note)
+app.route('/notes/<string:note_id>', methods=['PUT'])(update_note)
+app.route('/notes/<string:note_id>', methods=['DELETE'])(delete_note)
 
 
 @app.route("/")
+# Get all notes from the database
 def index():
-    return render_template("index.html")
+    return jsonify({"message": "Welcome to the Notes API"})

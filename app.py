343251e2aa9e9e30b9b635747008779c07ad8368
Moodyw03 <82095659+Moodyw03@users.noreply.github.com>
2023-12-01 13:13:05 +0000

    Update routes for Notes API

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ import datetime
 from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from bson import ObjectId
-from controllers.note_controller import create_note, get_notes, get_note, update_note, delete_note
+from controllers.note_controller import (
+    create_note,
+    get_notes,
+    get_note,
+    update_note,
+    delete_note,
+)
 
 
 from config.db import db
@@ -16,11 +22,11 @@ app.secret_key = os.getenv("SECRET_KEY")
 
 
 # CRUD Routes for Notes
-app.route('/notes', methods=['POST'])(create_note)
-app.route('/notes', methods=['GET'])(get_notes)
-app.route('/notes/<string:note_id>', methods=['GET'])(get_note)
-app.route('/notes/<string:note_id>', methods=['PUT'])(update_note)
-app.route('/notes/<string:note_id>', methods=['DELETE'])(delete_note)
+app.route("/api/notes", methods=["POST"])(create_note)
+app.route("/api/notes", methods=["GET"])(get_notes)
+app.route("/api/notes/<string:note_id>", methods=["GET"])(get_note)
+app.route("/api/notes/<string:note_id>", methods=["PUT"])(update_note)
+app.route("/api/notes/<string:note_id>", methods=["DELETE"])(delete_note)
 
 
 @app.route("/")

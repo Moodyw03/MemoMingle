85f7a2dd38ee04012615072bdf2d8d6d59391d86
Moodyw03 <82095659+Moodyw03@users.noreply.github.com>
2023-12-05 12:33:17 +0000

    Refactor auth routes and templates

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,45 +1,45 @@
 from flask import Blueprint, request, session, redirect, url_for, render_template
 from models.user import User
 
-auth = Blueprint('auth', __name__)
+auth = Blueprint("auth", __name__)
 
 
-@auth.route('/sign-in', methods=['GET', 'POST'])
+@auth.route("/sign-in", methods=["GET", "POST"])
 def sign_in():
     # if the user is already logged in, redirect to the notes page
-    if 'user_id' in session:
-        return redirect(url_for('note.get_notes'))
+    if "user_id" in session:
+        return redirect(url_for("note.get_notes"))
 
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
 
         if User.verify_password(username, password):
             user = User.find_by_username(username)
-            session['user_id'] = str(user['_id'])
-            return redirect(url_for('note.get_notes'))
-        return render_template('sign-in.html', error='Invalid credentials')
-    return render_template('sign-in.html')
+            session["user_id"] = str(user["_id"])
+            return redirect(url_for("note.get_notes"))
+        return render_template("sign-in.html", error="Invalid credentials")
+    return render_template("sign-in.html")
 
 
-@auth.route('/sign-up', methods=['GET', 'POST'])
+@auth.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
     # if the user is already logged in, redirect to the notes page
-    if 'user_id' in session:
-        return redirect(url_for('note.get_notes'))
+    if "user_id" in session:
+        return redirect(url_for("note.get_notes"))
 
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
         if User.find_by_username(username):
-            return render_template('auth/sign-up.html', error='Email already exists')
+            return render_template("sign-up.html", error="username already exists")
         user = User(username, password)
         user.insert()
-        return redirect(url_for('auth.sign_in'))
-    return render_template('sign-up.html')
+        return render_template("sign-in.html", success="Account created successfully")
+    return render_template("sign-up.html")
 
 
-@auth.route('/sign-out')
+@auth.route("/sign-out")
 def sign_out():
-    session.pop('user_id', None)
-    return redirect(url_for('auth.sign_in'))
+    session.pop("user_id", None)
+    return redirect(url_for("auth.sign_in"))

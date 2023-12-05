a5cea30a1d85ed844b5f0a92fcf618a36987a9e3
Moodyw03 <82095659+Moodyw03@users.noreply.github.com>
2023-12-05 09:26:40 +0000

    Refactor note controller routes and add login required decorator

--- controllers/note_controller.py
+++ controllers/note_controller.py
@@ -1,7 +1,10 @@
 from datetime import datetime
-from bson import ObjectId
-from flask import jsonify, request
+from flask import Blueprint, jsonify, request, session, redirect, url_for, render_template
+
 from models.note import Note
+from decorators import login_required
+
+note = Blueprint('note', __name__)
 
 
 def serialize(doc):
@@ -9,47 +12,94 @@ def serialize(doc):
     return doc
 
 
-def create_note():
-    data = request.json
-    note = Note(data['title'], data['content'], data.get('tags', []))
-    note_id = note.insert()
-    return jsonify({'message': 'Note created successfully', 'id': str(note_id)}), 201
-
-
+# Notes
+@note.route('/', methods=['GET'])
+@login_required
 def get_notes():
-    notes = Note.find_all()
-    return jsonify([serialize(note) for note in notes])
+    user_id = session['user_id']
+    if not user_id:
+        return redirect(url_for('auth.sign_in'))
+
+    # Fetch message from query parameters
+    success = request.args.get('success')
+    error = request.args.get('error')
+    notes = Note.find_all(user_id)
+    return render_template('notes.html', notes=notes, success=success, error=error)
 
 
-def get_note(note_id):
+# New Note
+@note.route('/new', methods=['GET', 'POST'])
+@login_required
+def create_note():
+    user_id = session['user_id']
+    if not user_id:
+        return redirect(url_for('auth.sign_in'))
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        tags = [tag.strip() for tag in request.form['tags'].split(';')]
+        note = Note(
+            title=title,
+            content=content,
+            tags=tags,
+            user_id=user_id,
+        )
+        note_id = note.insert()
+
+        success = None
+        error = None
+
+        if note_id:
+            success = 'Note created successfully'
+        else:
+            error = 'Failed to create note'
+        return redirect(url_for('note.get_notes', success=success, error=error))
+    return render_template('new-note.html')
+
+
+# DELETE /notes/<note_id>/delete
+@note.route('/<string:note_id>/delete', methods=['POST'])
+@login_required
+def delete_note(note_id):
+    user_id = session['user_id']
+    if not user_id:
+        return redirect(url_for('auth.sign_in'))
+
     try:
         note = Note.find_by_id(note_id)
-        if note:
-            return jsonify(serialize(note))
-
-        return jsonify({'message': 'Note not found'}), 404
-    except:
-        return jsonify({'message': 'Invalid note ID'}), 400
-
-
-def update_note(note_id):
-    data = request.json
-    data['lastModified'] = datetime.now()
-    try:
-        print(data)
-        if Note.find_by_id(note_id):
-            Note.update(note_id, data)
-            return jsonify({'message': 'Note updated successfully'})
-        return jsonify({'message': 'Note not found'}), 404
-    except:
-        return jsonify({'message': 'Invalid note ID'}), 400
-
-
-def delete_note(note_id):
-    try:
-        if Note.find_by_id(note_id):
+        if note and note['user_id'] == user_id:
             Note.delete(note_id)
-            return jsonify({'message': 'Note deleted successfully'})
-        return jsonify({'message': 'Note not found'}), 404
-    except:
-        return jsonify({'message': 'Invalid note ID'}), 400
+            return redirect(url_for('note.get_notes', success='Note deleted successfully'))
+        else:
+            return redirect(url_for('note.get_notes', error='Note not found')), 404
+    except Exception as e:
+        return redirect(url_for('note.get_notes', error='Invalid note ID')), 400
+
+
+# GET POST /notes/<note_id>/edit
+@note.route('/<string:note_id>/edit', methods=['GET', 'POST'])
+@login_required
+def edit_note(note_id):
+    note = Note.find_by_id(note_id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        tags = [tag.strip() for tag in request.form['tags'].split(';')]
+
+        # Create a dictionary for the new data
+        new_data = {
+            'title': title,
+            'content': content,
+            'tags': tags,
+        }
+
+        Note.update(note_id, new_data)  # Update the note in the DB
+        return redirect(url_for('note.get_notes', success='Note edited successfully'))
+
+    if note:
+        # Prepare tags for display
+        note['tags'] = '; '.join(note['tags'])
+
+    return render_template('edit-note.html', note=note)

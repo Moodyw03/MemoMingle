b92d415c545dd7d87804b16d15a708c9522521bc
Moodyw03 <82095659+Moodyw03@users.noreply.github.com>
2023-12-05 12:33:30 +0000

    Refactor Note model insert method and add search functionality

--- models/note.py
+++ models/note.py
@@ -14,29 +14,50 @@ class Note:
 
     def insert(self):
         # Set 'lastModified' to the current time on insert
-        result = db.notes.insert_one({
-            'title': self.title,
-            'content': self.content,
-            'dateCreated': self.dateCreated,
-            'lastModified': datetime.now(),
-            'tags': self.tags,
-            'user_id': self.user_id,
-        })
+        result = db.notes.insert_one(
+            {
+                "title": self.title,
+                "content": self.content,
+                "dateCreated": self.dateCreated,
+                "lastModified": datetime.now(),
+                "tags": self.tags,
+                "user_id": self.user_id,
+            }
+        )
         return result.inserted_id
 
     @staticmethod
     def find_all(user_id=None):
-        return db.notes.find({'user_id': user_id}).sort('lastModified', -1)
+        return db.notes.find({"user_id": user_id}).sort("lastModified", -1)
 
     @staticmethod
     def find_by_id(note_id):
-        return db.notes.find_one({'_id': ObjectId(note_id)})
+        return db.notes.find_one({"_id": ObjectId(note_id)})
 
     @staticmethod
     def update(note_id, new_data):
-        new_data['lastModified'] = datetime.now()
-        db.notes.update_one({'_id': ObjectId(note_id)}, {'$set': new_data})
+        new_data["lastModified"] = datetime.now()
+        db.notes.update_one({"_id": ObjectId(note_id)}, {"$set": new_data})
 
     @staticmethod
     def delete(note_id):
-        db.notes.delete_one({'_id': ObjectId(note_id)})
+        db.notes.delete_one({"_id": ObjectId(note_id)})
+
+    @staticmethod
+    def search(query, user_id=None):
+        return db.notes.find(
+            {
+                "$and": [
+                    {
+                        "$or": [
+                            {"title": {"$regex": query, "$options": "i"}},
+                            {"content": {"$regex": query, "$options": "i"}},
+                            {"tags": {"$regex": query, "$options": "i"}},
+                            {"dateCreated": {"$regex": query, "$options": "i"}},
+                            {"lastModified": {"$regex": query, "$options": "i"}},
+                        ]
+                    },
+                    {"user_id": user_id},
+                ]
+            }
+        ).sort("lastModified", -1)

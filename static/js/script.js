5fa7a22df0260fdf951c6d1c15153a78bb30aa26
Moodyw03 <82095659+Moodyw03@users.noreply.github.com>
2023-12-19 16:44:09 +0000

    Fix UI layout and add password validation function

--- static/js/script.js
+++ static/js/script.js
@@ -29,3 +29,15 @@ if (error) {
     removeParam("error");
   }, 3000);
 }
+
+// Check if the both passwords are the same
+function checkPasswords() {
+  var password = document.getElementById("password").value;
+  var confirmPassword = document.getElementById("confirm-password").value;
+
+  if (password !== confirmPassword) {
+    alert("Passwords do not match.");
+    return false; // Prevent form submission
+  }
+  return true;
+}

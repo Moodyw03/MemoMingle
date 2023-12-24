472a612ab90f91df5de0d1ef0949babef22e327a
Moodyw03 <82095659+Moodyw03@users.noreply.github.com>
2023-12-24 13:13:01 +0000

    Added styles

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
 
 
-app.register_blueprint(auth, url_prefix='/auth')
-app.register_blueprint(note, url_prefix='/notes')
+app.register_blueprint(auth, url_prefix="/auth")
+app.register_blueprint(note, url_prefix="/notes")
 
 
 @app.route("/")

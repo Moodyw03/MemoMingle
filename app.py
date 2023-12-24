74f54870a202340743776a4d12c0eb32d1d8838a
Moodyw03 <82095659+Moodyw03@users.noreply.github.com>
2023-12-24 17:00:28 +0000

    new line in note content

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, render_template
 
+from markupsafe import escape, Markup
+from jinja2 import pass_eval_context
+
 from controllers.auth_controller import auth
 from controllers.note_controller import note
 
@@ -9,6 +12,17 @@ load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
+
+
+@pass_eval_context
+def nl2br(eval_ctx, value):
+    result = escape(value).replace("\n", Markup("<br>\n"))
+    if eval_ctx.autoescape:
+        result = Markup(result)
+    return result
+
+
+app.jinja_env.filters["nl2br"] = nl2br
 
 
 app.register_blueprint(auth, url_prefix="/auth")

50dacfb0b5e234d818dd85318e6baecac2f9c07a
Moodyw03 <82095659+Moodyw03@users.noreply.github.com>
2024-01-07 10:28:58 +0000

    Add footer styles and modal functionality

--- static/js/script.js
+++ static/js/script.js
@@ -41,3 +41,31 @@ function checkPasswords() {
   }
   return true;
 }
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("modal-btn"); // Make sure you have a button with this ID in your HTML
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// Get the button element that closes the modal
+var cancel = document.getElementById("cancel");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on cancel, close the modal
+cancel.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
